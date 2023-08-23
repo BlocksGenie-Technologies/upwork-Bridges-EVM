97c24dcbc9de728c67e435c0f6d8d3bd3c3f84f1
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2023-08-23 20:32:48 +0500

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,18 +3,19 @@ const hre = require("hardhat");
 async function main() {
   const bridgeAddress = "0xb8901acB165ed027E32754E0FFe830802919727f";
 
-  //const ArbitrumBridge = await hre.ethers.getContractFactory("BridgeETHToArbitrumETH");
+  const ArbitrumBridge = await hre.ethers.getContractFactory("BridgeETHToArbitrumETH");
   const BaseBridge = await hre.ethers.getContractFactory("BridgeETHToBaseETH");
   const OptimismBridge = await hre.ethers.getContractFactory("BridgeETHToOptimismETH");
   const PolygonBridge = await hre.ethers.getContractFactory("BridgeETHToPolygonETH");
 
-  //const arbitrumBridge = await ArbitrumBridge.deploy(bridgeAddress);
+  const arbitrumBridge = await ArbitrumBridge.deploy(bridgeAddress);
   const baseBridge = await BaseBridge.deploy(bridgeAddress);
   const optimismBridge = await OptimismBridge.deploy(bridgeAddress);
   const polygonBridge = await PolygonBridge.deploy(bridgeAddress);
 
-//ArbitrumBridge Contract deployed to:", ${arbitrumBridge.address}
-  console.log(`BaseBridge Contract deployed to:", ${baseBridge.address},
+
+  console.log(`ArbitrumBridge Contract deployed to:", ${arbitrumBridge.address},
+              BaseBridge Contract deployed to:", ${baseBridge.address},
               OptimismBridge Contract deployed to:", ${optimismBridge.address},
               PolygonBridge Contract deployed to:", ${polygonBridge.address}`
   );
